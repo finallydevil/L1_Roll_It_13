45c29990547e881a10556251b6fa82be3684d2c6
finallydevil <mohammedn4063@masseyhigh.school.nz>
2026-02-05 13:23:11 +1300

    basic yes/no checker

--- C_01_yes_no_basic.py
+++ C_01_yes_no_basic.py
@@ -1,9 +1,15 @@
-want_instructions = input("Do you want to know the instructions?").lower
+while True:
 
-#check if user says yes or no
-if want_instructions == "yes":
-    print("You said yes!")
-elif want_instructions == "no":
-    print("You said no!")
-else:
-    print("please enter 'yes' or 'no'")
+    want_instructions = input("Do you want to know the instructions?").lower
+
+    #check if user says yes or no
+    if want_instructions == "yes" or want_instructions == "y":
+        print("You said yes!")
+        break
+    elif want_instructions == "no" or want_instructions == "n":
+        print("You said no!")
+        break
+    else:
+        print("please enter 'yes' or 'no'")
+        continue
+print("we done")
